59f360cf11a3b3075fb607e3af5ba5418d71fca5
martinisaiasabdala <125407812+martinisaiasabdala@users.noreply.github.com>
2023-02-14 22:05:41 -0300

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    const sideNav = document.body.querySelector('#sideNav');
+    if (sideNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#sideNav',
+            offset: 74,
+        });
+    };
+    
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
+
+document.getElementById('boton').addEventListener('click', function(){
+    console.log("Hola mundo desde EventListener");
+    document.getElementById("demo").innerHTML = "Enviado.";
+ });
